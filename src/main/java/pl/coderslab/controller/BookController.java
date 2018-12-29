a99c585d709800f58c13218e733838d9ee707838
Grzegorz Rogowski <rogowski.g@gmail.com>
2018-12-29 10:14:57 +0100

    classes BookController and MemoryBookService added

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,11 @@
+package pl.coderslab.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.model.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+}
